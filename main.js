ef0b7022dcfd7ef57948cb248375e9989a3ebaac
Angel Hernandez-Perez <angel.n.hernandez@gmail.com>
2017-08-07 05:07:05 -0700

    produces video and displays in window
    successfully uses native electron (chromium api) to display screen. Uses advanced selection process of getting user media that includes depreciated models and future models.

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 const electron = require('electron')
 // Module to control application life.
 const app = electron.app
+
+const globalShortcut = electron.globalShortcut
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
 const path = require('path')
 const url = require('url')
+
+
+//CommandorControl is used since the mac equivalent of control on a keyboard is command. See documentation under platform notice
+// PrintScreen is what is listed under available key codes
+const fixedshortcut = 'CmdOrCtrl+Shift+Plus'
+
+
+
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -13,7 +23,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 800, height: 600, darkTheme: true, title: 'Electron'})
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
@@ -23,7 +33,7 @@ function createWindow () {
   }))
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+   mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -34,10 +44,44 @@ function createWindow () {
   })
 }
 
+function removeGlobalShortcut(){
+   if (globalShortcut.isRegistered(fixedshortcut) == true) 
+    {globalShortcut.unregister(fixedshortcut)
+      console.log("shortcut is unregistered now")
+    }
+}
+
+function addGlobalShortcut () {
+  // check if shortcut registered for control shift print
+  // later on we'll allow for prompting of desired command
+  
+  if (globalShortcut.isRegistered(fixedshortcut))
+  {
+     console.log("The command " + fixedshortcut + "is unavailable.")
+  }
+  else
+  { //callback function is called when button is pressed
+    console.log("add global shortcut")
+    globalShortcut.register(fixedshortcut, () => {
+      console.log("print shortcut was pressed!!!")
+    })
+  }
+}
+
+
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+
+// app registers global shortcut for print screen only once when app is ready. not on each createwindow
+// not sure how to call multiple functions
+app.on('ready', //createWindow
+function (){ createWindow ()
+  removeGlobalShortcut()
+  addGlobalShortcut()
+ }
+  )
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -55,6 +99,11 @@ app.on('activate', function () {
     createWindow()
   }
 })
+
+app.on('will-quit', () => {
+  // Unregister printscreen shortcut.
+  removeGlobalShortcut()
+  })
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
--- renderer.js
+++ renderer.js
@@ -1,3 +1,143 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+
+const {desktopCapturer} = require('electron')
+const gifshot = require('gifshot')
+
+
+if (navigator.mediaDevices === undefined) {
+  navigator.mediaDevices = {};
+}
+
+
+
+
+
+
+
+
+
+// Some browsers partially implement mediaDevices. We can't just assign an object
+// with getUserMedia as it would overwrite existing properties.
+// Here, we will just add the getUserMedia property if it's missing.
+if (navigator.mediaDevices.getUserMedia === undefined) {
+  navigator.mediaDevices.getUserMedia = function(constraints) {
+
+    // First get ahold of the legacy getUserMedia, if present
+    var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+    // Some browsers just don't implement it - return a rejected promise with an error
+    // to keep a consistent interface
+    if (!getUserMedia) {
+      return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+    }
+
+    // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+    return new Promise(function(resolve, reject) {
+      getUserMedia.call(navigator, constraints, resolve, reject);
+    });
+  }
+}
+
+/// capture sources!!!
+desktopCapturer.getSources({types: ['window', 'screen']}, (error, sources) => {
+
+  //var pttrn = / ^Entire\sScreen$ | ^Screen\s\d*/
+console.log("getsources")
+  if (error) throw error
+  for (let i = 0; i <  sources.length; ++i) {
+    if (sources[i].name === 'Hello World!') {   // /^Entire\sScreen$/ | /^Screen\s\d*/
+      // navigator.webkitGetUserMedia({
+      //   audio: false,
+      //   video: {
+      //     mandatory: {
+      //       chromeMediaSource: 'desktop',
+      //       chromeMediaSourceId: sources[i].id,
+      //       minWidth: 1280,
+      //       maxWidth: 1280,
+      //       minHeight: 720,
+      //       maxHeight: 720
+      //     }
+      //   }
+      // }, handleStream, handleError)
+
+      var promise = navigator.mediaDevices.getUserMedia(
+            { audio: false,       video: {
+            mandatory: {
+              chromeMediaSource: 'desktop',
+              maxWidth: 1920,
+              maxHeight: 1080,
+              maxFrameRate: 10,
+              minAspectRatio: 1.77,
+              chromeMediaSourceId: sources[i].id         
+                       }
+              }
+            }
+          )
+          .then(function(stream) {
+
+            var video = document.querySelector('video');
+            // Older browsers may not have srcObject
+            if ("srcObject" in video) {
+              video.srcObject = stream;
+            } else {
+              // Avoid using this in new browsers, as it is going away.
+              video.src = URL.createObjectURL(stream);
+            }
+            video.onloadedmetadata = function(e) {
+              video.play();
+            };
+          })
+          .catch(function(err) {
+            console.log(err.name + ": " + err.message);
+          });
+      console.log('Promise is made')
+      return
+    }
+    else {      console.log('Sources that aren\'t the desired screen/window' + sources[i].name)
+    }
+  }
+})
+
+
+
+
+function clearphoto() {
+    var context = canvas.getContext('2d');
+    context.fillStyle = "#AAA";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    var data = canvas.toDataURL('image/png');
+    photo.setAttribute('src', data);
+  }
+
+function takepicture() {
+    var context = canvas.getContext('2d');
+    if (width && height) {
+      canvas.width = width;
+      canvas.height = height;
+      context.drawImage(video, 0, 0, width, height);
+    
+      var data = canvas.toDataURL('image/png');
+      photo.setAttribute('src', data);
+    } else {
+      clearphoto();
+    }
+  }
+
+
+function handleStream (stream) {
+  document.querySelector('video').src = URL.createObjectURL(stream)
+  //gifshot.takeSnapShot()
+  console.log("handleStream")
+}
+
+function handleError (e) {
+  console.log(e)
+}
+
+
+
+//handleStream("big_buck_bunny.mp4")
